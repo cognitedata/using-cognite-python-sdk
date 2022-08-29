ed4e50dadc7df57dfb58582407efec27a950d3a8
StigHaraldGustavsen <stig.harald.gustavsen@cognite.com>
2022-08-29 14:14:16 +0200

    updated auth, replaced sdk-core with sdk

--- utils/auth.py
+++ utils/auth.py
@@ -2,7 +2,8 @@
 import os
 from pathlib import Path
 
-from cognite.client import CogniteClient
+from cognite.client import ClientConfig, CogniteClient
+from cognite.client.credentials import OAuthClientCredentials, Token
 from dotenv import load_dotenv
 from msal import PublicClientApplication
 
@@ -17,6 +18,7 @@ dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")  # store secret in .env file
 
+BASE_URL = f"https://{CDF_CLUSTER}.cognitedata.com"
 SCOPES = [f"https://{CDF_CLUSTER}.cognitedata.com/.default"]
 
 AUTHORITY_HOST_URI = "https://login.microsoftonline.com"
@@ -30,14 +32,13 @@ def interactive_client():
     """Function to Create the Cognite Client, using Interactive Login method"""
     app = PublicClientApplication(client_id=CLIENT_ID, authority=AUTHORITY_URI)
     creds = app.acquire_token_interactive(scopes=SCOPES, port=PORT)
-    client = CogniteClient(
-        token_url=creds["id_token_claims"]["iss"],
-        token=creds["access_token"],
-        token_client_id=creds["id_token_claims"]["aud"],
+    cnf = ClientConfig(
+        client_name="my-special-client",
         project=COGNITE_PROJECT,
-        base_url=f"https://{CDF_CLUSTER}.cognitedata.com",
-        client_name="cognite-client-interactive",
+        credentials=Token(creds["access_token"]),
+        base_url=BASE_URL,
     )
+    client = CogniteClient(cnf)
     return client
 
 
@@ -47,29 +48,31 @@ def device_code_client():
     device_flow = app.initiate_device_flow(scopes=SCOPES)
     print(device_flow["message"])  # print device code to screen
     creds = app.acquire_token_by_device_flow(flow=device_flow)
-    client = CogniteClient(
-        token_url=creds["id_token_claims"]["iss"],
-        token=creds["access_token"],
-        token_client_id=creds["id_token_claims"]["aud"],
+    cnf = ClientConfig(
+        client_name="my-special-client",
         project=COGNITE_PROJECT,
-        base_url=f"https://{CDF_CLUSTER}.cognitedata.com",
-        client_name="cognite-client-device",
+        credentials=Token(creds["access_token"]),
+        base_url=BASE_URL,
     )
+    client = CogniteClient(cnf)
     return client
 
 
 def client_secret_client():
     """Function to Create the Cognite Client, using Credentials (e.g. ClientID, Client secret)"""
-    client = CogniteClient(
+    creds = OAuthClientCredentials(
         token_url=TOKEN_URL,
-        token_client_id=CLIENT_ID,
-        token_client_secret=CLIENT_SECRET,
-        token_scopes=SCOPES,
-        project=COGNITE_PROJECT,
-        base_url=f"https://{CDF_CLUSTER}.cognitedata.com",
-        client_name="client_secret_test_script",
-        debug=False,
+        client_id=CLIENT_ID,
+        scopes=SCOPES,
+        client_secret=CLIENT_SECRET,
     )
+    cnf = ClientConfig(
+        client_name="my-special-client",
+        project=COGNITE_PROJECT,
+        credentials=creds,
+        base_url=BASE_URL,
+    )
+    client = CogniteClient(cnf)
     return client
 
 
